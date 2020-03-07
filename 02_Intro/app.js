dbf897ce48ac2728dd2b4e1d29824c894bc41163
Mario Lazzari <mario.lazzari@gmail.com>
2020-03-07 18:18:40 +0100

    Done lesson 6

--- 02_Intro/app.js
+++ 02_Intro/app.js
@@ -1,1 +1,23 @@
-console.log("app");
+// clear console
+
+// log to console
+console.log("Section 02: intro.");
+console.log(123);
+console.log(true);
+console.log([1, 2, 3]);
+
+// log obj
+const obj = { id: 1, name: "mario" };
+console.log(obj);
+console.table(obj);
+
+// error logging
+console.error("this is an error.");
+console.warn("this is a warning.");
+
+// console time
+console.time("for");
+for (let i = 0; i < 1000; i++) {
+  console.log("time");
+}
+console.timeEnd("for");

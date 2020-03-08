e80591545555e138f2859b406ddb23c30ee20a78
Mario Lazzari <mario.lazzari@gmail.com>
2020-03-08 14:17:06 +0100

    Done lesson 7

--- 02_Intro/app.js
+++ 02_Intro/app.js
@@ -21,3 +21,35 @@ for (let i = 0; i < 1000; i++) {
   console.log("time");
 }
 console.timeEnd("for");
+
+// var
+/*
+var name = "Mario Lazzari";
+console.log(name);
+name = "Maria Lazzari";
+console.log(name);
+*/
+
+// let
+let name;
+console.log("name", name);
+name = "Mario Lazzari";
+console.log(name);
+name = "Maria Lazzari";
+console.log(name);
+
+// const
+const pi = 3.14;
+console.log("pi:", pi);
+//pi = 0 -> error
+const person = { name: "Mario" };
+console.log("Person:", person);
+person.name = "Maria";
+console.log("Person:", person);
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+numbers.push(6);
+console.log(numbers);
+numbers.unshift(0);
+console.log(numbers);
+// numbers = [] -> error

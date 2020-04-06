9e9cdf9bc8769aed65fedbefc25ce4374b0cde7f
Mario Lazzari <mario.lazzari@gmail.com>
2020-04-06 19:51:14 +0200

    Done lesson 24

--- 03_Dom/app.js
+++ 03_Dom/app.js
@@ -28,3 +28,36 @@ console.log("Script src:", document.scripts[2].getAttribute("src"));
 // cast html collection to array
 let scripts = Array.from(document.scripts);
 scripts.forEach((script) => console.log(script.getAttribute("src")));
+
+// get element by id
+console.log("Task title:", document.getElementById("task-title"));
+console.log("Task title ID:", document.getElementById("task-title").id);
+console.log(
+  "Task title Class:",
+  document.getElementById("task-title").className
+);
+
+// change style
+document.getElementById("task-title").style.background = "#333";
+document.getElementById("task-title").style.color = "#fff";
+document.getElementById("task-title").style.padding = "5px";
+//document.getElementById("task-title").style.display = "none";
+
+// change content
+document.getElementById("task-title").textContent = "Task list";
+document.getElementById("task-title").innerHTML =
+  '<span style="color:red">Tasks list</span>';
+
+// query selector
+console.log("Title id:", document.querySelector("#task-title"));
+console.log("Task class:", document.querySelector(".card-title"));
+console.log("First H5:", document.querySelector("h5"));
+// nested classes
+document.querySelector("ul li").style.color = "blue";
+// pseudo classes
+document.querySelector("ul li:last-child").style.color = "red";
+document.querySelector("ul li:nth-child(3").style.color = "yellow";
+document.querySelector("ul li:nth-child(4").textContent = "4th task";
+
+// query selector all
+console.log("Total tasks:", document.querySelectorAll("li").length);
